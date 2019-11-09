2c02ee2a31ab47f6a16e0d5a622848b1b0e641f3
Kris Immig <kris.immig@gmail.com>
2019-11-09 15:29:47 +0100

    Add svg loader

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('svg-sprite')
+      .use('svgo-loader')
+      .loader('svgo-loader')
+      .options({
+        plugins: [
+          {removeAttrs: {attrs:['fill', 'stroke']}}
+        ]
+      })
+  }
+}
