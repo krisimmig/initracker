4769aacf8270fbf6b5b80592114c53663defa428
Kris Immig <kris.immig@gmail.com>
2020-10-03 14:06:37 +0200

    Add button disabled and better copy

--- src/components/encounters/EncounterNew.vue
+++ src/components/encounters/EncounterNew.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="EncounterNew card shadow">
-    <h4>Create a new encounter</h4>
-    <p>An encounter is a battle that your party has to survive to reach there goals in your campaign. You can prepare as many encounters here as you want and stat them whenever it fits into your narrative. </p>
+    <h4 class="h4">Create a new encounter</h4>
+    <p>An encounter is a battle that your party has to survive to reach their goals in your campaign. You can prepare as many encounters here as you want and stat them whenever it fits into your narrative. </p>
     <FormInput
-      class="mb-3"
-      placeholder="enter a name here..."
+      class="mb-3 mt-3"
+      placeholder="for example 'The burning planes'"
       label="Encounter name"
       v-model="encounterName"
       @keyup.enter="submitNewEncounter"
     />
-    <Button is-success @click="submitNewEncounter">Save</Button>
+    <Button is-success :disabled="this.encounterName === ''" @click="submitNewEncounter">Add encounter</Button>
   </div>
 </template>
 
@@ -29,6 +29,8 @@ export default class EncounterNew extends Vue {
   public encounterName: string = '';
 
   public submitNewEncounter(): void {
+    if (this.encounterName === '') return;
+
     dispatchAddNewEncounter(this.$store, {
       encounterName: this.encounterName,
     });
