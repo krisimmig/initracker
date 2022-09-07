8259b9017c0b70aa8543dbf11ab48e8d658b7c8b
Kris Immig <kris.immig@gmail.com>
2022-09-07 12:30:37 +0200

    Yarn upgrade, cleanup

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,3 @@
-// postcss.config.js
-const purgecss = require('@fullhuman/postcss-purgecss')({
-
-  // Specify the paths to all of the template files in your project
-  content: [
-    './src/**/*.html',
-    './src/**/*.vue',
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
-
 const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
 
@@ -18,8 +5,5 @@ module.exports = {
   plugins: [
     tailwindcss,
     autoprefixer,
-    ...process.env.NODE_ENV === 'production'
-        ? [purgecss]
-        : []
   ],
 };
