3a38f6fe5098712bcba63f64d557f61a98fb670c
Kris Immig <kris.immig@gmail.com>
2026-02-28 14:47:00 +0100

    Simplify page title

--- src/components/common/PageTitle.vue
+++ src/components/common/PageTitle.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="py-3">
-    <div class="d-flex align-center gap-3">
-      <v-icon v-if="icon" :icon="icon" size="x-large" color="secondary" />
-      <h1 class="text-h4 text-md-h2 mb-0 ml-4">{{ title }}</h1>
+  <div class="mb-4">
+    <div class="d-flex align-center ga-3">
+      <v-icon v-if="icon" :icon="icon" size="small" color="primary" />
+      <h1 class="text-h6 font-weight-bold mb-0">{{ title }}</h1>
     </div>
-    <p class="text-subtitle-1" v-if="subtitle">{{ subtitle }}</p>
   </div>
 </template>
 
