ef8f62a4bc1928a81217e6d8f3fe1932457e4ed5
Kris Immig <kris.immig@gmail.com>
2026-02-28 13:24:29 +0100

    Rework condition modal

--- src/components/characters/common/CharacterCondition.vue
+++ src/components/characters/common/CharacterCondition.vue
@@ -1,105 +1,117 @@
 <template>
   <div class="NpcCondition mt-3">
-    <v-btn
+    <v-chip
       v-for="(condition, index) in conditions"
-      @click="setSelectedCondition(condition.id)"
       :key="index"
       size="small"
-      variant="outlined"
       color="secondary"
+      variant="outlined"
       class="mr-1 mb-1"
+      @click="setSelectedCondition(condition.id)"
     >
       {{ condition.name }}
-      <span v-if="condition.duration" class="ml-1">
-        ({{ condition.duration }})
-      </span>
-    </v-btn>
+      <span v-if="condition.duration" class="ml-1">({{ condition.duration }})</span>
+    </v-chip>
 
     <v-btn
-      @click="showConditionSelect = true"
       size="small"
       variant="text"
       color="primary"
       class="mb-1"
+      @click="showConditionSelect = true"
     >
       <v-icon start>mdi-plus</v-icon>
-      <template v-if="conditions.length < 4"> condition</template>
+      <template v-if="conditions.length < 4">condition</template>
     </v-btn>
 
-    <v-dialog v-model="showConditionSelect" width="min(80vw, 900px)">
+    <v-dialog v-model="showConditionSelect" max-width="860">
       <v-card>
-        <v-app-bar flat>
-          <v-toolbar-title>Set this characters status effects</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn icon @click.stop="showConditionSelect = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-app-bar>
+        <v-card-title class="d-flex align-center justify-space-between pt-4 px-4">
+          <span>Status Effects</span>
+          <v-btn icon="mdi-close" variant="text" @click="showConditionSelect = false" />
+        </v-card-title>
+        <v-card-subtitle class="px-4 pb-2">{{ name }}</v-card-subtitle>
 
-        <v-card-text style="height: 500px;">
-          <v-container>
-            <v-row>
-              <v-col>
-                <div style="column-count: 2">
-                  <div v-for="condition in npcConditions" :key="condition.id">
-                    <v-btn
-                      size="small"
-                      variant="text"
-                      @click="selectedCondition = condition"
-                      :color="isActiveCondition(condition.id) ? 'primary' : ''"
-                      :class="[{'text-decoration-underline': selectedCondition?.id === condition.id}, 'mb-2 mr-2']"
-                    >
-                      {{ condition.name }}
-                    </v-btn>
-                  </div>
-                </div>
-              </v-col>
-              <v-col>
-                <template v-if="selectedCondition">
-                  <h4 class="title">{{ selectedCondition.name }}</h4>
-                  <ul class="u-list mb-4 text-body-1">
-                    <li v-for="(effect, index) in selectedCondition.effects" :key="index">{{ effect }}</li>
-                  </ul>
-                </template>
-                <template v-else>
-                  <v-alert type="info" variant="outlined">
-                    Select a condition from the list to see more info.
-                  </v-alert>
-                </template>
-              </v-col>
-            </v-row>
-          </v-container>
+        <v-divider />
+
+        <v-card-text class="pa-4 pb-2">
+          <!-- Word cloud -->
+          <div class="d-flex flex-wrap ga-2 mb-4">
+            <v-chip
+              v-for="condition in npcConditions"
+              :key="condition.id"
+              :color="selectedCondition?.id === condition.id ? 'secondary' : isActiveCondition(condition.id) ? 'primary' : undefined"
+              :variant="selectedCondition?.id === condition.id || isActiveCondition(condition.id) ? 'tonal' : 'outlined'"
+              :prepend-icon="isActiveCondition(condition.id) ? 'mdi-check-circle' : undefined"
+              size="default"
+              @click="selectedCondition = condition"
+            >
+              {{ condition.name }}
+            </v-chip>
+          </div>
+
+          <v-divider class="mb-4" />
+
+          <!-- Condition detail -->
+          <div style="min-height: 120px;">
+            <template v-if="selectedCondition">
+              <p class="text-subtitle-1 font-weight-bold mb-2">{{ selectedCondition.name }}</p>
+              <p
+                v-for="(effect, i) in selectedCondition.effects"
+                :key="i"
+                class="text-body-2 mb-2"
+              >{{ effect }}</p>
+            </template>
+            <template v-else>
+              <v-alert type="info" variant="tonal">
+                Select a condition above to see its effects.
+              </v-alert>
+            </template>
+          </div>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
+
+        <v-divider />
+
+        <v-card-actions class="pa-4">
           <template v-if="selectedCondition">
-            <div v-if="isActiveCondition(selectedCondition.id)">
+            <template v-if="isActiveCondition(selectedCondition.id)">
+              <v-spacer />
               <v-btn
-                color="primary"
+                color="error"
+                variant="tonal"
+                prepend-icon="mdi-minus-circle"
                 @click="removeCondition(selectedCondition.id)"
-                class="mb-5 mr-3"
               >
-                <v-icon start>mdi-minus</v-icon>
                 Remove {{ selectedCondition.name }}
               </v-btn>
-            </div>
-            <div v-else class="d-flex align-middle">
+            </template>
+            <template v-else>
               <v-text-field
                 v-model="selectedCondition.duration"
-                label="Duration (in Rounds)"
-                class="pt-0 mt-0"
-                prepend-inner-icon="mdi-clock"
-                hint="Leave empty for permanent conditions"
+                label="Duration (rounds)"
+                density="compact"
+                variant="outlined"
+                prepend-inner-icon="mdi-clock-outline"
+                hint="Leave empty for permanent"
+                persistent-hint
+                hide-details="auto"
+                style="max-width: 220px;"
+                type="number"
               />
+              <v-spacer />
               <v-btn
                 color="primary"
-                style="width: 200px; margin: 0 1rem;"
+                variant="tonal"
+                prepend-icon="mdi-plus-circle"
                 @click="addCondition(selectedCondition)"
               >
-                <v-icon start>mdi-plus</v-icon>
                 Add {{ selectedCondition.name }}
               </v-btn>
-            </div>
+            </template>
+          </template>
+          <template v-else>
+            <v-spacer />
+            <v-btn variant="text" @click="showConditionSelect = false">Close</v-btn>
           </template>
         </v-card-actions>
       </v-card>
@@ -172,34 +184,4 @@ function removeCondition(conditionId: string) {
 }
 </script>
 
-<style>
-.NpcCondition-condition,
-.NpcCondition-add {
-  font-size: .8em;
-  color: theme('colors.blue.600');
-  cursor: pointer;
-  margin-right: .7em;
-  border: 1px solid theme('colors.blue.400');;
-  border-radius: 1em;
-  height: 1.4rem;
-  display: inline-block;
-  text-align: center;
-  padding: 0 7px 3px 7px;
-}
 
-.NpcCondition-addText {
-  display: inline-block;
-}
-
-.NpcCondition-condition {
-  margin-right: .7em;
-  cursor: pointer;
-}
-
-.NpcCondition-add:hover,
-.NpcCondition-condition:hover {
-  color: theme('colors.blue.100');
-  border-color: theme('colors.blue.500');
-  background-color: theme('colors.blue.600');
-}
-</style>
