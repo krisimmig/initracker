2cdb9553cddd01c5ceab98f0c29e35900d61eaf4
Kris Immig <kris.immig@gmail.com>
2026-02-28 12:34:51 +0100

    Refactor new char elements

--- src/views/CharactersView.vue
+++ src/views/CharactersView.vue
@@ -3,7 +3,53 @@
     <PageTitle title="Characters" subtitle="Create or edit your own monsters and characters" icon="mdi-account-group" />
 
     <v-row>
-      <v-col>
+      <!-- New character: first on mobile, right column on md+ -->
+      <v-col cols="12" md="4" :order="1" :order-md="2">
+        <v-card variant="outlined">
+          <v-card-title class="d-flex align-center pa-3 pb-0">
+            <v-icon color="primary" class="mr-2">mdi-account-plus</v-icon>
+            <span class="text-subtitle-1 font-weight-bold">New character</span>
+          </v-card-title>
+
+          <v-card-text class="pa-3">
+            <p class="text-body-2 text-medium-emphasis mb-3">
+              Create a new character from scratch or base it on an existing one from the library.
+            </p>
+            <v-btn
+              color="primary"
+              variant="flat"
+              prepend-icon="mdi-account-plus"
+              block
+              :to="{ name: 'characterEdit', params: { type: 'base-empty' } }"
+            >
+              New character sheet
+            </v-btn>
+          </v-card-text>
+
+          <v-card-actions class="pa-3 pt-0">
+            <v-btn
+              color="primary"
+              variant="outlined"
+              prepend-icon="mdi-account-multiple-plus"
+              block
+              @click="showCharacterLibrary = true"
+            >
+              Open library
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-dialog v-model="showCharacterLibrary" max-width="80vw">
+          <CharacterLibrary
+            @characterClicked="handleCharacterClicked"
+            @closeClicked="showCharacterLibrary = false"
+            buttonText="Use as base"
+          />
+        </v-dialog>
+      </v-col>
+
+      <!-- Character list: second on mobile, left column on md+ -->
+      <v-col cols="12" md="8" :order="2" :order-md="1">
         <div v-if="isLoading">
           <v-card class="mb-3">
             <v-skeleton-loader class="mx-auto" type="heading, list-item-three-line" />
@@ -28,37 +74,6 @@
           </template>
           <v-alert variant="outlined" type="info" v-else>No custom characters found.</v-alert>
         </template>
-      </v-col>
-
-      <v-col cols="4">
-        <v-card>
-          <v-card-text class="text-body-1 text-center">
-            <p>Create a new character from scratch:</p>
-            <router-link :to="{ name: 'characterEdit', params: { type: 'base-empty' } }" style="text-decoration: none; color: inherit;">
-              <v-btn color="primary">
-                <v-icon start>mdi-account-plus</v-icon>
-                New character sheet
-              </v-btn>
-            </router-link>
-            <div class="d-flex my-4 align-center">
-              <v-divider />
-              <span class="px-4 font-weight-bold">or</span>
-              <v-divider />
-            </div>
-            <p>Base your new character on an existing one from the library:</p>
-            <v-btn @click="showCharacterLibrary = true" color="primary">
-              <v-icon start>mdi-account-multiple-plus</v-icon>
-              Open library
-            </v-btn>
-          </v-card-text>
-          <v-dialog v-model="showCharacterLibrary" max-width="80vw">
-            <CharacterLibrary
-              @characterClicked="handleCharacterClicked"
-              @closeClicked="showCharacterLibrary = false"
-              buttonText="Use as base"
-            />
-          </v-dialog>
-        </v-card>
       </v-col>
     </v-row>
   </div>
